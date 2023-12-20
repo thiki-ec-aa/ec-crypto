4bcd6e5c975a069983b1eadc40382dc183be355e
Joe Zeng <zhu.zeng@xuncetech.com>
2023-12-20 11:19:14 +0800

    decrypt script

--- src/assembly/bin/decrypt.sh
+++ src/assembly/bin/decrypt.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/bash
 set -x
-java -jar crypter-boot.jar --private-key-file=private.key --zip-file-name=bundles.zip --target=bundles decrypt
+keyfile=hxlc-xc.key
+
+datestr=$1
+
+zipfile=xc-v1.1.4-$datestr.zip
+
+OIFS=$IFS
+IFS=$'\n'
+echo "enter the password:"
+read -s passwd
+IFS=$OIFS
+
+unzip -P $passwd ${keyfile}.zip
+java -jar crypter-boot.jar --private-key-file=${keyfile} --zip-file-name="${zipfile}" --target=$datestr decrypt
 set +x
+
+# clearning
+rm $keyfile
+rm "$datestr/*.zip" "$datestr/token.txt"
+mv $datestr/$datestr/* $datestr
+rmdir $datestr/$datestr
+
+echo "Press any key to continue"
+read -n 1
 
