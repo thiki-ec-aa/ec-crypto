71a5727c01e493e38f7389de19b5bc23847f07fd
Joe Zeng <zhu.zeng@xuncetech.com>
2024-03-25 11:11:27 +0800

    update script

---
+++ src/assembly/bin/update-crypter.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+## update crypter jar
+cd $(dirname $0)
+
+if [ "$1" = '' ] ;then
+    echo "need parameter to specify crypter install parent directory."
+    exit 1
+fi
+jar_name=crypter.jar
+install_root=$base/.crypter
+
+if [ ! -d "$install_root"  ] ; then
+  mkdir "$install_root"
+fi
+
+cp -f ../${jar_name} "$install_root"/
+cp -f ./crypter-function.sh "$install_root"/
+
+echo crypter is updated.
+
+
